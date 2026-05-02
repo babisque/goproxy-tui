003a8980df416ab0cbad7a06dfcb83ebf7b72402
babisque <rodrigobabisque1@gmail.com>
2026-05-02 09:24:23 -0300

    feat: implement request forwarding in ProxyHandler

--- internal/proxy/handler.go
+++ internal/proxy/handler.go
@@ -2,12 +2,30 @@ package proxy
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 type ProxyHandler struct{}
 
 func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.RequestURI = ""
 	fmt.Println(r.Method, r.Host)
-	w.Write([]byte("Intercepted by Go!"))
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		fmt.Println("Error forwarding request:", err)
+		http.Error(w, "Error forwarding request", http.StatusBadGateway)
+		return
+	}
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+	defer resp.Body.Close()
 }
