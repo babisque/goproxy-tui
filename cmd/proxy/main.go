73e6d129ed750a4abe63431349ff23847b7bd49c
babisque <rodrigobabisque1@gmail.com>
2026-05-02 09:41:20 -0300

    feat: add TUI application structure and initial implementation

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/babisque/goproxy-tui/internal/proxy"
+	"github.com/babisque/goproxy-tui/internal/tui"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	proxyHandler := proxy.ProxyHandler{}
+	// proxyHandler := proxy.ProxyHandler{}
 
-	fmt.Println("Starting proxy server on :8080")
+	// fmt.Println("Starting proxy server on :8080")
 
-	err := http.ListenAndServe(":8080", &proxyHandler)
-	if err != nil {
-		log.Fatal("Error starting server:", err)
+	// err := http.ListenAndServe(":8080", &proxyHandler)
+	// if err != nil {
+	// 	log.Fatal("Error starting server:", err)
+	// }
+
+	app := tui.NewApp()
+	if _, err := tea.NewProgram(app, tea.WithAltScreen()).Run(); err != nil {
+		log.Fatal("Error running TUI:", err)
 	}
 }
---
+++ internal/tui/app.go
@@ -0,0 +1,69 @@
+package tui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	colorGray  = lipgloss.Color("#555555")
+	colorWhite = lipgloss.Color("#FFFFFF")
+
+	boxStyle = lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder(), true).
+			BorderForeground(colorGray).
+			Padding(1, 2)
+)
+
+type App struct {
+	width  int
+	height int
+}
+
+func NewApp() App {
+	return App{}
+}
+
+func (a App) Init() tea.Cmd {
+	return nil
+}
+
+func (a App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		a.width = msg.Width
+		a.height = msg.Height
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return a, tea.Quit
+		}
+	}
+	return a, nil
+}
+
+func (a App) View() string {
+	if a.width == 0 {
+		return "Loading..."
+	}
+
+	boxHeight := a.height - 4
+	leftWidth := (a.width / 100) * 30
+	rightWidth := a.width - leftWidth - 6
+
+	leftBox := boxStyle.Copy().
+		Width(leftWidth).
+		Height(boxHeight).
+		Render("Requests list\n\n(Empty)")
+
+	rightBox := boxStyle.Copy().
+		Width(rightWidth).
+		Height(boxHeight).
+		Render("Request details\n\n(Empty)")
+
+	ui := lipgloss.JoinHorizontal(lipgloss.Top, leftBox, rightBox)
+
+	return lipgloss.NewStyle().Margin(1, 2).Render(ui)
+}
