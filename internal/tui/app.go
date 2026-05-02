7d9048d0eff311151921bf9c6a40da7264ae1057
babisque <rodrigobabisque1@gmail.com>
2026-05-02 10:04:59 -0300

    feat: enhance TUI app to display request logs and improve initialization

--- internal/tui/app.go
+++ internal/tui/app.go
@@ -1,6 +1,9 @@
 package tui
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/babisque/goproxy-tui/internal/proxy"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -20,7 +23,10 @@ type App struct {
 	width      int
 	height     int
 	logChannel chan proxy.RequestLog
+	requests   []proxy.RequestLog
 }
+
+type logMsg proxy.RequestLog
 
 func NewApp(ch chan proxy.RequestLog) App {
 	return App{
@@ -29,11 +35,15 @@ func NewApp(ch chan proxy.RequestLog) App {
 }
 
 func (a App) Init() tea.Cmd {
-	return nil
+	return waitForLog(a.logChannel)
 }
 
 func (a App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case logMsg:
+		a.requests = append(a.requests, proxy.RequestLog(msg))
+
+		return a, waitForLog(a.logChannel)
 
 	case tea.WindowSizeMsg:
 		a.width = msg.Width
@@ -57,10 +67,27 @@ func (a App) View() string {
 	leftWidth := (a.width / 100) * 30
 	rightWidth := a.width - leftWidth - 6
 
+	var listBuilder strings.Builder
+	listBuilder.WriteString("Requests list\n\n")
+
+	if len(a.requests) == 0 {
+		listBuilder.WriteString("(Empty)")
+	} else {
+		for i, req := range a.requests {
+			row := fmt.Sprintf("[%d] %s %s\n", req.Status, req.Method, req.URL)
+			listBuilder.WriteString(row)
+
+			if i > boxHeight-8 {
+				listBuilder.WriteString("... (more requests)\n")
+				break
+			}
+		}
+	}
+
 	leftBox := boxStyle.Copy().
 		Width(leftWidth).
 		Height(boxHeight).
-		Render("Requests list\n\n(Empty)")
+		Render(listBuilder.String())
 
 	rightBox := boxStyle.Copy().
 		Width(rightWidth).
@@ -70,4 +97,11 @@ func (a App) View() string {
 	ui := lipgloss.JoinHorizontal(lipgloss.Top, leftBox, rightBox)
 
 	return lipgloss.NewStyle().Margin(1, 2).Render(ui)
+}
+
+func waitForLog(ch chan proxy.RequestLog) tea.Cmd {
+	return func() tea.Msg {
+		log := <-ch
+		return logMsg(log)
+	}
 }
