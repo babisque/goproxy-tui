b3c2d43527f72e6aeb78dcedde32cad20ac9b3aa
babisque <rodrigobabisque1@gmail.com>
2026-05-04 11:07:09 -0300

    refactor: streamline response handling and logging in proxy server

--- internal/proxy/handler.go
+++ internal/proxy/handler.go
@@ -35,19 +35,12 @@ func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			Headers: http.Header{},
 			Body:    "Blocked by proxy rules",
 		}
-
 		return
 	}
 
 	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
 		http.Error(w, "Error forwarding request", http.StatusBadGateway)
-		return
-	}
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Error reading response body", http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
@@ -58,18 +51,27 @@ func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	resp.Body = io.NopCloser(io.MultiReader(io.NopCloser(bytes.NewBuffer(bodyBytes)), resp.Body))
+	if _, ignore := ph.IgnoredDomains[r.Host]; ignore {
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
+		return
+	}
 
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Error reading response body", http.StatusInternalServerError)
+		return
+	}
+
+	resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 
-	if _, ignore := ph.IgnoredDomains[r.Host]; !ignore {
-		ph.LogChannel <- RequestLog{
-			Method:  r.Method,
-			URL:     r.Host + r.URL.Path,
-			Status:  resp.StatusCode,
-			Headers: resp.Header.Clone(),
-			Body:    string(bodyBytes),
-		}
+	ph.LogChannel <- RequestLog{
+		Method:  r.Method,
+		URL:     r.Host + r.URL.Path,
+		Status:  resp.StatusCode,
+		Headers: resp.Header.Clone(),
+		Body:    string(bodyBytes),
 	}
 }
